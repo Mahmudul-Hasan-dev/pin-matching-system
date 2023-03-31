4c3785eb84c3ff2d34f85decc5093133a4a46e47
Mahmudul Hasan <contact.mahmudul.h@gmail.com>
2023-03-31 22:03:07 +0600

    pin generated

--- js/app.js
+++ js/app.js
@@ -1,7 +1,43 @@
 // console.log('app connected')
 
-function gereatePin() {
-    const pin = Math.round(Math.random() * 10000);
-    return pin;
+// function varifyPin() {
+//     const pincheck = gereatePin();
+//     const pincheckstring = pincheck + '';
+//     if (pincheckstring === 4) {
+//         return pincheck;
+//     }
+//     else {
+//         return varifyPin();
+//     }
+// }
+
+// function gereatePin() {
+//     const pin = Math.round(Math.random() * 1000)
+//     return pin;
+// }
+
+///////////////////////////////////////////////////////
+function getPin() {
+    const pin = generatePin();
+    const pinString = pin + '';
+    if (pinString.length === 4) {
+        return pin;
+    }
+    else {
+        // console.log('pin not 3 digit found', pin);
+        return getPin();
+    }
 }
-gereatePin();
+
+function generatePin() {
+    const random = Math.round(Math.random() * 10000);
+    return random;
+}
+
+
+document.getElementById('generate-btn').addEventListener('click', function () {
+    const pin = getPin();
+    // console.log(pin);
+    const displayfield = document.getElementById('display-field');
+    displayfield.value = pin;
+})
