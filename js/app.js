1443b731b3435306c4951f7110cd79e8287333a9
Mahmudul Hasan <contact.mahmudul.h@gmail.com>
2023-04-01 16:59:24 +0600

    done message given

--- js/app.js
+++ js/app.js
@@ -41,3 +41,58 @@ document.getElementById('generate-btn').addEventListener('click', function () {
     const displayfield = document.getElementById('display-field');
     displayfield.value = pin;
 })
+
+//event delegate(event on group of similar element)
+document.getElementById('calculator').addEventListener('click', function (event) {
+    // console.log(event.target.innerText);
+    const number = event.target.innerText;
+    const displayField = document.getElementById('number-field');
+    const prevNumber = displayField.value;
+
+    if (isNaN(number)) {
+        // console.log(number);
+        if (number === 'C') {
+            displayField.value = '';
+        }
+        else if (number === '<') {
+            const digits = prevNumber.split('');
+            digits.pop();
+            const remaineddigits = digits.join('');
+            displayField.value = remaineddigits;
+        }
+    }
+    else {
+        const newNumber = prevNumber + number;
+        displayField.value = newNumber;
+    }
+})
+
+document.getElementById('verify-pin').addEventListener('click', function () {
+    // console.log('submit clcik')
+
+    //get generated pin
+    const genereateddisplayField = document.getElementById('display-field')
+    const generatedPin = genereateddisplayField.value;
+
+    //get user input
+    const typedPinField = document.getElementById('number-field');
+    const typedPin = typedPinField.value;
+
+    //we have to access message from outside of block element to get them in both condition
+    const pinsuccessMessage = document.getElementById('pin-success');
+    const pinfailMessage = document.getElementById('pin-fail');
+
+    //compare both
+    if (generatedPin == typedPin) {
+        // console.log('correct')
+
+        pinsuccessMessage.style.display = 'block';
+        pinfailMessage.style.display = 'none';
+    }
+    else {
+        // console.log('incorrect')
+
+        pinfailMessage.style.display = 'block';
+        pinsuccessMessage.style.display = 'none';
+    }
+})
